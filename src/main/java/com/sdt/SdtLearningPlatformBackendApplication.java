f102f3b74a681b6047209749a38a5df9ab3bdc2a
Hussenvalli <hussenvallik@gmail.com>
2026-02-22 11:46:51 +0530

    intial commit

---
+++ src/main/java/com/sdt/SdtLearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtLearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdtLearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdt/SdtLearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtLearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
